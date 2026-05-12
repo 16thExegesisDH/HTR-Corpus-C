b73203b540255d819636d7a0f8a56926b5d7930c
Dupuis Béatrice <la.bea.dupuis@gmail.com>
2026-05-12 08:59:00 +0200

    Python Erasmus

--- data/lxml_altoid.py
+++ data/lxml_altoid.py
@@ -1,8 +1,8 @@
 import glob
 from lxml import etree
 
-# forme du code : folder_path = "Hemmingsen_Rm/*.xml"
-folder_path = "Bugenhagen_rm/*.xml"
+# forme du code : folder_path = "Erasmus_1-2-Cor"
+folder_path = "Erasmus_1-2-Cor/*.xml"
 
 ALTO_NS = "http://www.loc.gov/standards/alto/ns-v4#"
 NSMAP = {"alto": ALTO_NS}
